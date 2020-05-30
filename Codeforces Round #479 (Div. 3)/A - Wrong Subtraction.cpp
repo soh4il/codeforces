d51db1ee0c98c8412ff724abe70d7d2f62504fce
Sohail Mahmud <sohailmahmud@yahoo.com>
2020-05-30 19:30:39 +0600

    Create A - Wrong Subtraction.cpp

---
+++ Codeforces Round #479 (Div. 3)/A - Wrong Subtraction.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int n,k;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //--------------------------
+    scanf("%d%d",&n,&k);
+    while(k--)
+    {
+        if(n%10)n--;
+        else n/=10;
+    }
+    printf("%d\n",n);
+    return 0;
+}
