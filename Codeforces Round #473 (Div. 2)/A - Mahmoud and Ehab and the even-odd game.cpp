d4ca8798a3535cfb124cac2a63e5e54c53581d39
Sohail Mahmud <sohailmahmud@yahoo.com>
2020-05-30 19:30:35 +0600

    Create A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ Codeforces Round #473 (Div. 2)/A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int n;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //--------------------------
+    scanf("%d",&n);
+    printf("%s\n",n%2?"Ehab":"Mahmoud");
+    return 0;
+}
